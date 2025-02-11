8be8e80ac9e0214a82a5a1ca027370e02576b0b4
Rayappu Thanusiyan <157779212+Thanusiyan1007@users.noreply.github.com>
2025-02-11 21:24:36 +0530

    deploy try 5

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,17 +1,7 @@
-"""
-ASGI config for backend project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
 
-settings_module = 'api.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'api.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+settings_module = "Hasthtag.deployment_settings" if "RENDER_EXTERNAL_HOSTNAME" in os.environ else "Hasthtag.settings"
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 application = get_asgi_application()
--- backend/wsgi.py
+++ backend/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'api.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'api.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+settings_module = "Hasthtag.deployment_settings" if "RENDER_EXTERNAL_HOSTNAME" in os.environ else "Hasthtag.settings"
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 application = get_wsgi_application()
