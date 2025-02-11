8d5f01788c95aaba9133b407c8aedafa51705001
Rayappu Thanusiyan <157779212+Thanusiyan1007@users.noreply.github.com>
2025-02-11 21:47:16 +0530

    deploy try 8

--- backend/asgi.py
+++ backend/asgi.py
@@ -4,4 +4,5 @@ from django.core.asgi import get_asgi_application
 settings_module = "backend.deployment_settings" if "RENDER_EXTERNAL_HOSTNAME" in os.environ else "backend.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
+
 application = get_asgi_application()
